78b140560e5714e44bb3f3b405b33524a842bda9
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-10-03 10:56:41 +0530

    Finished problem 1.
    Signed-off-by: Noufal Ibrahim <noufal@nibrahim.net.in>

---
+++ 1.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut cntr = 1;
+    let mut sum = 0;
+    while (cntr < 1000) {
+        if (cntr % 3 == 0 || cntr % 5 == 0) {
+            sum += cntr;
+        }
+        cntr+=1;
+    }
+    println(fmt!("%d\n", sum));
+}
